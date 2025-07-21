3958702b0a4bd8db2cc67360e5a4d9aa04ee7a94
princegamer137 <princesahanigamer99@gmail.com>
2025-07-21 15:05:26 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+const sessionId = crypto.randomUUID();
+localStorage.setItem('sessionId', sessionId);
 const app = createApp(App)
 
 app.use(createPinia())
